7956d6bd344abbbef0b1a6331c233c94f647355e
Jaime <jaimeloeuf@gmail.com>
2023-07-05 02:41:38 +0800

    Add Product Info card for portal home view.

--- portal/src/views/core/Home.vue
+++ portal/src/views/core/Home.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import SideDrawer from "../components/SideDrawer.vue";
 import Chart from "./Chart.vue";
-import MIT from "./MIT.vue";
+import ProductCard from "./KeyInfoCard/ProductCard.vue";
 import SprintNumberCard from "./KeyInfoCard/SprintNumberCard.vue";
 import PMFScoreCard from "./KeyInfoCard/PMFScoreCard.vue";
+import MIT from "./MIT.vue";
 </script>
 
 <template>
@@ -13,13 +14,15 @@ import PMFScoreCard from "./KeyInfoCard/PMFScoreCard.vue";
       <span class="ml-4 text-4xl">Home</span>
     </div>
 
-    <!-- Row of key info cards -->
-    <div class="m-12 flex flex-row justify-end space-x-6">
-      <MIT />
-      <SprintNumberCard />
-      <PMFScoreCard />
-    </div>
+    <div>
+      <div class="m-12 flex flex-row justify-end space-x-6">
+        <MIT />
+        <SprintNumberCard />
+        <PMFScoreCard />
+        <ProductCard />
+      </div>
 
-    <Chart />
+      <Chart />
+    </div>
   </div>
 </template>
---
+++ portal/src/views/core/KeyInfoCard/ProductCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const product = { name: "Starterkit" };
+</script>
+
+<template>
+  <div class="inline-block w-full rounded-lg bg-slate-50 p-4 shadow">
+    <p class="text-sm font-medium">Product</p>
+
+    <div class="text-right">
+      <p class="text-4xl font-light">
+        {{ product.name }}
+      </p>
+    </div>
+  </div>
+</template>
