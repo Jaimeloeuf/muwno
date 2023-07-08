51d36ab738195fc16eae946280519259ca2a3178
Jaime <jaimeloeuf@gmail.com>
2023-07-09 00:11:45 +0800

    Show empty message if MIT card is empty.

--- portal/src/views/core/MIT.vue
+++ portal/src/views/core/MIT.vue
@@ -11,6 +11,7 @@ const MITs = [
     <div class="flex flex-row items-center justify-between">
       <p class="text-sm font-semibold">MIT (Most Important Tasks)</p>
       <router-link
+        v-if="MITs.length !== 0"
         :to="{}"
         class="rounded-lg bg-lime-500 p-1 px-3 text-sm font-semibold text-gray-50"
       >
@@ -18,7 +19,11 @@ const MITs = [
       </router-link>
     </div>
 
-    <div>
+    <div v-if="MITs.length === 0" class="mt-3 text-2xl font-light">
+      This will be automatically generated once there are survey responses.
+    </div>
+
+    <div v-else>
       <router-link
         v-for="(MIT, i) in MITs"
         :key="MIT"
