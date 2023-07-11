95fd5f41a0840da105d99c019a4210794490c474
Jaime <jaimeloeuf@gmail.com>
2023-07-11 12:29:35 +0800

    Update 404 pages for both Portal/ and Form/.

--- portal/src/views/404.vue
+++ portal/src/views/404.vue
@@ -1,57 +1,36 @@
 <script setup lang="ts">
-import { HomeRoute } from "../router";
-
-const loc = window.location.toString();
+import { OrgRoute } from "../router";
 </script>
 
 <template>
-  <div class="columns is-multiline mx-6 my-6">
-    <div class="column is-full">
-      <p class="title is-3">Page Not Found</p>
-      <br />
+  <section class="bg-white">
+    <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+      <div class="mx-auto max-w-screen-sm text-center">
+        <img src="../assets/404.svg" />
 
-      <p class="subtitle">"{{ loc }}"</p>
+        <h1
+          class="mb-4 text-7xl font-extrabold tracking-tight text-blue-600 lg:text-9xl"
+        >
+          404
+        </h1>
 
-      <div class="columns is-vcentered">
-        <div class="column">
-          <p class="subtitle">
-            The page you tried to look for does not exists!
-          </p>
-        </div>
+        <p
+          class="mb-6 text-2xl font-bold tracking-tight text-gray-900 md:text-4xl"
+        >
+          Whoops! This page doesn't exist.
+        </p>
 
-        <div class="column is-narrow">
-          <!-- @todo -->
-          <a
-            href=""
-            target="_blank"
-            class="button is-light is-warning is-fullwidth"
-          >
-            report issue
-          </a>
-        </div>
+        <p class="mt-11 text-lg font-light text-gray-500">
+          Here are some helpful links instead:
+        </p>
+
+        <router-link
+          :to="{ name: OrgRoute.name }"
+          class="my-4 inline-flex text-center font-medium text-gray-500 underline"
+        >
+          Home
+        </router-link>
       </div>
     </div>
-
-    <div class="column is-full">
-      <hr />
-    </div>
-
-    <div class="column is-half">
-      <button
-        class="button is-light is-warning is-fullwidth"
-        @click="$router.back"
-      >
-        Back
-      </button>
-    </div>
-
-    <div class="column is-half">
-      <router-link
-        class="button is-light is-success is-fullwidth"
-        :to="{ name: HomeRoute.name }"
-      >
-        Home
-      </router-link>
-    </div>
-  </div>
+  </section>
 </template>
