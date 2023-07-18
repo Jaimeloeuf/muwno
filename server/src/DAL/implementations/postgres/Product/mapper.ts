e7cab49a8f57a152527405d2cdcda4aadd1acf3f
Jaime <jaimeloeuf@gmail.com>
2023-07-18 22:50:23 +0800

    Calculate current sprint number with dayjs lib.

--- server/src/DAL/implementations/postgres/Product/mapper.ts
+++ server/src/DAL/implementations/postgres/Product/mapper.ts
@@ -1,5 +1,25 @@
 import type { product as ProductModel } from '@prisma/client';
 import type { Products } from 'domain-model';
+import dayjs from 'dayjs';
+
+/**
+ * Utility function to calculate current sprint number of a `Product` using its
+ * `createdAt` timestamp as the start of its first sprint, and `daysPerSprint`
+ * value as each sprint's interval.
+ */
+function getCurrentSprintNumber(productModel: ProductModel) {
+  const today = dayjs();
+  const productFirstSprintStartDate = dayjs(productModel.createdAt);
+  const daysSinceFirstSprint = today.diff(
+    productFirstSprintStartDate,
+    'day',
+    true,
+  );
+  const currentSprintNumber = Math.trunc(
+    daysSinceFirstSprint / productModel.daysPerSprint,
+  );
+  return currentSprintNumber;
+}
 
 export function mapProductModelToEntity(
   productModels: Array<ProductModel>,
@@ -11,10 +31,10 @@ export function mapProductModelToEntity(
       id: productModel.id,
       createdAt: productModel.createdAt.toISOString(),
       name: productModel.name,
+      currentSprint: getCurrentSprintNumber(productModel),
 
       // @todo Fix these values
       score: 1,
-      currentSprint: 1,
       samplingDetails: {
         rate: 1,
         size: 1,
