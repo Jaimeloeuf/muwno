cabeca2c5b07c077d3df96cc22e76965e70c63db
Jaime <jaimeloeuf@gmail.com>
2023-07-22 01:10:07 +0800

    Recreate GlobalError view:     Recreate Global Error view to use tailwind, and duplicate the logic     across all the Vue JS subrepos.

--- landing/src/views/GlobalError.vue
+++ landing/src/views/GlobalError.vue
@@ -1,33 +1,72 @@
 <script setup lang="ts">
+import { HomeRoute } from "../router";
+
 defineProps<{ globalError: Error }>();
 
 defineEmits(["acknowledged"]);
 </script>
 
 <template>
-  <div class="section">
-    <div class="columns is-mobile is-multiline">
-      <div class="column is-full">
-        <h2 class="title">Page Error!</h2>
-      </div>
+  <section class="bg-white">
+    <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+      <div class="mx-auto max-w-screen-sm text-center">
+        <div class="flex flex-row justify-center">
+          <img src="../assets/404.svg" class="max-h-52 md:max-h-96" />
+        </div>
 
-      <div class="column is-narrow">
-        <!-- @todo -->
-        <a href="" class="button is-light is-warning is-fullwidth"> report </a>
-      </div>
-
-      <div class="column">
-        <button
-          class="button is-light is-success is-fullwidth"
-          @click="$emit('acknowledged')"
+        <h1
+          class="mb-4 text-7xl font-extrabold tracking-tight text-blue-600 lg:text-9xl"
         >
-          back
-        </button>
-      </div>
+          whoops
+        </h1>
 
-      <div class="column is-full">
-        <div class="box" style="word-wrap: break-word">{{ globalError }}</div>
+        <p
+          class="mb-6 text-2xl font-bold tracking-tight text-gray-900 md:text-4xl"
+        >
+          This page BROKE!
+        </p>
+
+        <p class="mt-11 text-lg font-light text-gray-500">
+          Reloading the page might fix it, if not please help us report this
+          issue!
+        </p>
+
+        <div class="my-6">
+          <!-- @todo Add link -->
+          <a
+            href=""
+            class="rounded-lg bg-lime-500 px-6 py-2 text-xl text-white"
+          >
+            REPORT 💛
+          </a>
+        </div>
+
+        <div class="my-6 flex flex-row justify-center space-x-6">
+          <button
+            class="rounded-lg bg-slate-200 px-6 py-2 text-xl text-gray-500"
+            @click="$emit('acknowledged')"
+          >
+            Back
+          </button>
+
+          <router-link
+            :to="{ name: HomeRoute.name }"
+            class="rounded-lg bg-slate-200 px-6 py-2 text-xl text-gray-500"
+          >
+            Home
+          </router-link>
+        </div>
+
+        <div class="text-left">
+          <p class="text-2xl font-extrabold">Error Message</p>
+          <div
+            class="w-full rounded-lg bg-slate-50 p-4 text-red-700 shadow-lg"
+            style="word-wrap: break-word"
+          >
+            {{ globalError }}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
