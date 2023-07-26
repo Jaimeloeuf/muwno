de0ac6a378afcc9eadbe1a99529c7834d92efbe1
Jaime <jaimeloeuf@gmail.com>
2023-07-26 20:08:35 +0800

    WIP Update AddProduct view for survey modes:     WIP Replace sampling details input in AddProduct view with a new     survey mode choice cards for users to select, since not all survey     modes will require sampling details.

--- portal/src/views/core/AddProduct.vue
+++ portal/src/views/core/AddProduct.vue
@@ -1,36 +1,73 @@
 <script setup lang="ts">
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { useOrg } from "../../store";
-import { ImportCustomerRoute } from "../../router";
+import { useOrg, useLoader } from "../../store";
+import { ProductRoute } from "../../router";
 import SideDrawer from "../components/SideDrawer.vue";
-import type { Product } from "domain-model";
+
+import type { SurveyMode } from "domain-model";
+import SurveyModeCard from "./SurveyModeCard.vue";
 
 const router = useRouter();
 const orgStore = useOrg();
+const loaderStore = useLoader();
 
-const name = ref("");
-const daysPerSprint = ref(14);
-const sampling = reactive<Product["samplingDetails"]>({
-  rate: 1,
-  size: 0,
-  maxSampleCount: 1,
-  coolOff: 4,
-});
+const name = ref<string>("");
 
-async function addProduct() {
+async function addProduct(surveyModeID: SurveyMode["id"]) {
   // @todo Check inputs
+
+  loaderStore.show();
 
   // @todo Call store method
   const { id } = await orgStore.createNewProduct({
     name: name.value,
-
-    // @todo This should be calculated and done at next page rather than here...
-    samplingDetails: sampling,
   });
 
-  router.push({ name: ImportCustomerRoute.name, params: { productID: id } });
+  if (surveyModeID === 1) {
+    // router.push({ name: ProductRoute.name, params: { productID: id } });
+    // Need to go import customers first
+  } else if (surveyModeID === 2) {
+    router.push({ name: ProductRoute.name, params: { productID: id } });
+  }
+
+  loaderStore.hide();
 }
+
+const surveyModes: Array<SurveyMode> = [
+  {
+    id: 1,
+    name: "Automatically Recurring",
+    productType: "Software as a Service (SaaS)",
+    productExamples: ["Spotify", "Netflix", "Instagram"],
+    usecase: [
+      "Mainly used for doing automatic and recurring surveying following product sprint/feature-release cycles.",
+      "Use this if you want to have a quick feedback loop/cycle for active product development.",
+    ],
+    descriptions: [
+      "This will automatically send out surveys for you without your intervention",
+      "Note that for this to work you will need to either integrate your system to automatically upload emails of your customers continuously or manually update them reguarly",
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Passive/Manual",
+    productType: "Physical Products",
+    productExamples: ["Innisfree", "Sephora", "Cereals"],
+    usecase: [
+      "Mainly used for getting customer feedbacks without reaching out through email surveys.",
+      "More of a customers reach out to do the survey",
+      "You can also used for one off campaigns",
+      "Use this if you do not have access to your customer's email for email blasts.",
+    ],
+    descriptions: [
+      "With Passive mode, you create a survey link + QR code and you disseminate to your customers however you want",
+      "As and when the customers decide to fill in your survey, you will get results",
+      "With Passive mode, you can do one off email survey blasts, by manually importing in customers' email",
+    ],
+  },
+];
 </script>
 
 <template>
@@ -40,8 +77,7 @@ async function addProduct() {
       <span class="ml-4 text-4xl">Add new Product</span>
     </div>
 
-    <div class="mx-auto w-full max-w-md">
-      <!-- Product Name input -->
+    <div class="mx-auto w-full max-w-lg">
       <div class="mb-10">
         <label>
           <p class="text-3xl">Product Name</p>
@@ -56,18 +92,14 @@ async function addProduct() {
         </label>
       </div>
 
-      <!-- Number of days per sprint input -->
-      <div class="mb-10">
-        <label>
-          <p class="text-3xl">Number of Days per Sprint</p>
-          <ul class="list-decimal px-5">
-            <li>How many days is your product / feature release cycle?</li>
-            <li>
-              Your customers will be surveyed using a multiple of this number of
-              days.
-            </li>
-            <li>Defaults to 14 days for a biweekly sprint cycle.</li>
-            <li>Minimum 7 days per sprint.</li>
+      <div class="mb-12">
+        <p class="text-3xl">Choose Survey Mode</p>
+        <ul class="list-decimal px-5">
+          <li>
+            Choose a <b>Survey Mode</b> based on the type of your product and
+            how you want to gather feedback.
+          </li>
+          </ul>
           </ul>
 
           <input
@@ -78,121 +110,28 @@ async function addProduct() {
             placeholder="Number of Days"
           />
         </label>
-      </div>
-
-      <!-- @todo
-          There should be a button for user to choose automatic or use deault
-
-          In fact by default the default values will be used, you should only
-          change them later in settings if you want to!
-        -->
-      <!-- Sampling Details section header -->
-      <div class="mb-6 border-t pt-4">
-        <p class="text-3xl">Sampling Details</p>
-      </div>
-
-      <!-- Sampling Rate input -->
-      <div class="mb-10">
-        <label>
-          <p class="text-xl">Rate</p>
-          <ul class="list-decimal px-5">
-            <li>Rate of sampling / surveying your customers.</li>
-            <li>
-              This is a multiple of your sprint, i.e. 1 means survey your
-              customers after every single sprint and 2 means survey your
-              customers once every 2 sprints.
-            </li>
-            <li>By default this samples once every sprint.</li>
-          </ul>
+        </ul>
 
           <input
-            v-model="sampling.rate"
+            v-model="daysPerSprint"
             type="number"
-            min="1"
+            min="7"
             class="mt-4 w-full rounded-lg border border-gray-300 bg-slate-50 p-6"
-            placeholder="Number of Sprints"
-          />
-        </label>
-
-        <!-- Calculated sample period value -->
-        <div class="mt-4">
-          <p class="text-lg">Sampling Period</p>
-          <div class="w-full rounded-lg bg-slate-100 p-3 font-extralight">
-            <p>
-              Sampling Rate:
-              <span class="font-normal">{{ sampling.rate }}</span>
-            </p>
-            <p class="ml-10">x</p>
-            <p>
-              Days per Sprint:
-              <span class="font-normal">{{ daysPerSprint }}</span> days
-            </p>
-
-            <p class="mt-2 border-t border-slate-300 pt-1 font-normal">
-              Every
-              {{ sampling.rate * daysPerSprint }} days
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Max Sample Count per user input -->
-      <div class="mb-10">
-        <label>
-          <p class="text-xl">Max Sample Count</p>
-          <ul class="list-decimal px-5">
-            <li>
-              What is the maximum number of times this tool can sample a single
-              customer?
-            </li>
-            <li>
-              A customer is considered sampled once they receive a survey link,
-              even if they do not open or complete the survey.
-            </li>
-          </ul>
-
-          <input
-            v-model="sampling.maxSampleCount"
-            type="number"
-            min="1"
-            class="mt-4 w-full rounded-lg border border-gray-300 bg-slate-50 p-6"
-            placeholder="Max Sample Count per customer"
+            placeholder="Number of Days"
           />
         </label>
       </div>
+    </div>
 
-      <!-- Max Sample Count per user input -->
-      <div class="mb-10">
-        <label>
-          <p class="text-xl">Cool off between samples</p>
-          <ul class="list-decimal px-5">
-            <li>
-              After every sample there should be a cool off time period (in
-              number of sprints) before the same user is sampled again, to
-              prevent sampling the same user over and over again.
-            </li>
-            <li>
-              How long (in sprints) should we wait before we sample the same
-              customer again.
-            </li>
-          </ul>
-
-          <input
-            v-model="sampling.coolOff"
-            type="number"
-            min="1"
-            class="mt-4 w-full rounded-lg border border-gray-300 bg-slate-50 p-6"
-            placeholder="Number of Sprints"
-          />
-        </label>
-      </div>
-
-      <button
-        class="w-full rounded-lg bg-green-600 p-4 text-xl text-white shadow"
-        @click="addProduct"
-      >
-        Add
-      </button>
+    <div
+      class="flex flex-col gap-6 sm:flex-row sm:flex-wrap sm:items-stretch sm:justify-center"
+    >
+      <SurveyModeCard
+        v-for="surveyMode in surveyModes"
+        :key="surveyMode.id"
+        :surveyMode="surveyMode"
+        @click="addProduct(surveyMode.id)"
+      />
     </div>
   </div>
 </template>
