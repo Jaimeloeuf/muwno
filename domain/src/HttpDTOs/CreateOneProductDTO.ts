59d1f4dee6a37bda4d382735a710a2f612d56165
Jaime <jaimeloeuf@gmail.com>
2023-07-27 22:24:20 +0800

    Add in missing DTOs.

---
+++ domain/src/HttpDTOs/CreateOneProductDTO.ts
@@ -0,0 +1,9 @@
+import type { Product, SurveyMode } from "../Product/index.js";
+
+/**
+ * DTO data used to create a single Product.
+ */
+export type CreateOneProductDTO = {
+  name: Product["name"];
+  surveyMode: SurveyMode["id"];
+};
