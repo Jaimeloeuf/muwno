a7eec2bf35e84706952d1ac0e67d4058a9988f4a
Jaime <jaimeloeuf@gmail.com>
2023-07-30 04:23:11 +0800

    Fix landing page formatting and responsive layout.

--- landing/src/views/Home.vue
+++ landing/src/views/Home.vue
@@ -3,291 +3,235 @@
 </script>
 
 <template>
-  <div class="bg-slate-50">
-    <div class="flex flex-row justify-between items-center md:p-4 p-6">
-      <img src="../assets/logo.svg" class="md:w-52 md:ml-20" />
+  <!-- Navbar -->
+  <div
+    class="flex flex-row items-center justify-between bg-slate-50 p-4 lg:px-20"
+  >
+    <img src="../assets/logo.svg" class="sm:w-52" />
 
-      <div>
-        <button class="mr-6 bg-green-800 h-12 text-white py-3 px-8 rounded-3xl">
-          Login
-        </button>
+    <div>
+      <button class="mr-6 rounded-3xl bg-green-800 px-8 py-2 text-white">
+        Login
+      </button>
 
-        <button
-          class="mr-6 h-12 text-black py-3 px-8 rounded-3xl border-2 border-green-800 hidden md:inline-block"
-        >
-          Sign up
-        </button>
-      </div>
+      <button
+        class="hidden rounded-3xl border-2 border-green-800 px-8 py-2 text-black md:inline-block"
+      >
+        Sign up
+      </button>
     </div>
   </div>
 
-  <div class="flex flex-row">
-    <div class="bg-slate-200 py-10 md:py-32 md:px-6 md:w-1/2">
-      <p
-        class="text-green-800 text-2xl text-center md:px-16 md:text-left md:text-5xl md:leading-snug md:pb-3"
-      >
-        We tell you what your <br />
-        customers would pay for
+  <!-- Hero section -->
+  <div
+    class="flex flex-row items-center justify-between bg-slate-200 px-10 py-16 md:px-24 lg:px-32"
+  >
+    <div class="max-w-sm md:max-w-full md:basis-1/2">
+      <p class="mb-4 text-3xl text-green-800 md:pb-3 md:text-4xl lg:text-5xl">
+        We tell you what your customers would pay for
       </p>
 
-      <p
-        class="text-sm text-center p-8 pt-3 pb-3 mb-4 md:text-2xl md:px-16 md:text-left md:pb-6"
-      >
+      <p class="mb-4 md:text-lg lg:text-2xl">
         Measure progress towards Product Market Fit and get actionable insights
         with a PMF survey.
       </p>
 
-      <div
-        class="flex flex-col space-y-4 items-center md:flex-row md:space-y-0 md:space-x-4 md:px-16"
-      >
-        <button
-          class="bg-green-800 text-white py-2 px-7 rounded-3xl w-30 md:h-12"
+      <div>
+        <div
+          class="flex max-w-lg flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0"
         >
-          Create Survey
-        </button>
-        <button
-          class="bg-white text-black border-2 border-green-800 py-2 px-7 rounded-3xl w-40 md:h-12"
-        >
-          Live Demo
-        </button>
+          <button class="w-full rounded-3xl bg-green-800 px-7 py-2 text-white">
+            Start
+          </button>
+          <button
+            class="w-full rounded-3xl border-2 border-green-800 bg-white px-7 py-2 text-black"
+          >
+            Live Demo
+          </button>
+        </div>
       </div>
     </div>
 
-    <div class="hidden bg-slate-200 md:w-1/2 md:py-24 md:inline-block">
-      <img src="../assets/hero.svg" class="w-auto" />
+    <div class="hidden bg-slate-200 md:inline-block md:basis-1/2">
+      <img src="../assets/hero.svg" />
     </div>
   </div>
 
-  <div class="md:flex flex-row">
-    <div class="bg-slate-100 py-6 md:py-20 md:w-2/5">
-      <p
-        class="text-green-800 text-2xl text-center md:px-24 md:text-left md:text-4xl md:leading-snug md:pb-3"
-      >
-        Product Market Fit (PMF)
-      </p>
-    </div>
+  <!-- PMF Description section -->
+  <div
+    class="flex flex-col bg-slate-100 p-10 md:px-24 md:py-20 lg:flex-row lg:space-x-12 lg:px-32"
+  >
+    <p class="mb-4 w-full text-2xl text-green-800 md:text-4xl">
+      Product Market Fit (PMF)
+    </p>
 
-    <div class="bg-slate-100 md:px-24 md:py-24 md:w-3/5">
-      <p
-        class="text-sm text-center p-8 pt-0 md:px-16 md:text-left md:text-xl md:leading-snug"
-      >
+    <div class="md:text-xl">
+      <p class="mb-2">
         PMF is having a <b>great product in the right market</b>, giving you
-        high demand and ultimately <b>success</b> of your business. <br /><br />
+        high demand and ultimately <b>success</b> of your business.
+      </p>
+      <p>
         You've hit PMF when <b>40% or more</b> of your customers say they will
         be very disappointed if they cannot use your product anymore.
       </p>
     </div>
   </div>
 
-  <div class="md:flex flex-row">
-    <div class="bg-slate-200 py-10 md:py-32 md:px-6 md:w-1/2">
-      <p
-        class="text-green-800 text-2xl text-center pb-3 md:px-16 md:text-left md:text-4xl md:leading-snug md:pb-3"
-      >
+  <div
+    class="flex flex-col justify-between bg-slate-200 p-10 md:flex-row md:px-24 md:py-20 md:pb-0 lg:p-32 lg:pb-0"
+  >
+    <div class="mb-12 w-full md:mb-0">
+      <p class="mb-2 text-2xl text-green-800 md:text-3xl lg:text-4xl">
         How theprodtool works
       </p>
 
-      <p
-        class="hidden md:inline-block visible text-base text-center p-5 mb-4 md:text-base md:px-16 md:text-left md:pb-2"
-      >
-        01/
-      </p>
+      <p class="mb-4">Part 1</p>
 
-      <p
-        class="text-base text-center md:px-16 md:text-left md:text-3xl md:pb-6"
-      >
+      <p class="mb-2 text-xl md:text-2xl lg:text-3xl">
         Automatic Survey Collection
       </p>
 
-      <p
-        class="text-sm text-center p-8 pt-3 md:px-16 md:text-left md:text-xl md:leading-snug md:pb-8"
-      >
-        Ditch your multiple tools (Google Forms+ Mailchimp+ Google Sheets+
-        Tableau) setup. <br /><br />
+      <p class="mb-1">
+        Ditch your multiple tools (Google Forms + Mailchimp + Google Sheets +
+        Tableau) setup.
+      </p>
+
+      <p class="mb-6">
         Intergrate your system with our tool to automatically survey your users
         to get feedback consistently and recurringly.
       </p>
 
-      <div
-        class="flex flex-col space-y-4 items-center md:flex-row md:space-y-0 md:space-x-4 md:px-16"
+      <button
+        class="w-48 rounded-3xl bg-green-800 px-6 py-2 text-lg text-slate-50"
       >
-        <button
-          class="bg-green-800 text-white py-2 px-7 rounded-3xl w-40 md:h-12"
-        >
-          Create Survey
-        </button>
-      </div>
+        Create Survey
+      </button>
     </div>
-    <div class="bg-slate-200 md:w-1/2 md:py-24">
-      <img src="../assets/hero.svg" class="w-auto" />
+
+    <div class="w-full max-w-lg md:max-w-full">
+      <img src="../assets/hero.svg" />
     </div>
   </div>
 
-  <div class="md:flex flex-row">
-    <div class="hidden bg-slate-200 md:w-1/2 md:py-24 md:inline-block">
-      <img src="../assets/hero.svg" class="w-auto" />
+  <div
+    class="flex flex-col-reverse justify-between bg-slate-200 p-10 pt-0 md:flex-row md:px-24 md:py-20 lg:p-32"
+  >
+    <div class="w-full max-w-lg md:mb-0 md:max-w-full">
+      <img src="../assets/hero.svg" />
     </div>
 
-    <div class="bg-slate-200 py-10 md:py-32 md:px-6 md:w-1/2">
-      <p></p>
+    <div class="mb-12 w-full">
+      <p class="mb-4">Part 2</p>
 
-      <p
-        class="hidden md:inline-block visible text-base text-center p-5 mb-4 md:text-base md:px-16 md:text-left md:pb-2"
-      >
-        /02
-      </p>
+      <p class="mb-2 text-xl md:text-2xl lg:text-3xl">Data Analysis</p>
 
-      <p
-        class="text-base text-center md:px-16 md:text-left md:text-3xl md:pb-6"
-      >
-        Data Analysis
-      </p>
-
-      <p
-        class="text-sm text-center p-8 pt-3 md:px-16 md:text-left md:text-xl md:leading-snug md:pb-6"
-      >
+      <p class="mb-6">
         Know your PMF progress over time. Get actionable items with the help of
         our AI model that analyses your customers' feedback.
       </p>
 
+      <button
+        class="w-48 rounded-3xl bg-green-800 px-6 py-2 text-lg text-slate-50"
+      >
+        View Dashboard
+      </button>
+    </div>
+  </div>
+
+  <div class="bg-slate-100 px-10 py-20 md:px-24 lg:px-32">
+    <div class="max-w-2xl">
+      <p class="text-2xl text-green-800 md:text-3xl lg:text-4xl">
+        The PMF Survey has successfully helped companies like these grow
+        <b>really fast.</b>
+      </p>
+    </div>
+
+    <div class="grid grid-cols-3">
+      <img src="../assets/dropbox.svg" />
+      <img src="../assets/eventbrite.svg" />
+      <img src="../assets/superhuman.svg" />
+    </div>
+  </div>
+
+  <div
+    class="flex flex-col space-y-6 bg-slate-200 px-10 py-16 md:flex-row md:space-x-12 md:px-16 lg:px-32"
+  >
+    <p class="mb-3 w-full text-2xl text-green-800 md:text-3xl lg:text-4xl">
+      Reach out to us when you are ready to growth hack your product to PMF!
+    </p>
+
+    <div class="flex w-full flex-col px-3 text-sm">
       <div
-        class="flex flex-col space-y-4 items-center md:flex-row md:space-y-0 md:space-x-4 md:px-16"
+        class="mb-6 flex flex-col space-y-6 sm:flex-row sm:space-x-4 sm:space-y-0"
       >
-        <button
-          class="bg-green-800 text-white py-2 px-7 rounded-3xl md:w-44 md:h-12"
-        >
-          View Dashboard
-        </button>
+        <label class="w-full tracking-wide">
+          <p class="mb-2 font-bold text-gray-700">FIRST NAME</p>
+
+          <input
+            class="w-full appearance-none rounded border bg-gray-300 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+            type="text"
+            placeholder="Jane"
+          />
+        </label>
+
+        <label class="w-full tracking-wide">
+          <p class="mb-2 font-bold text-gray-700">LAST NAME</p>
+
+          <input
+            class="w-full appearance-none rounded border bg-gray-300 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+            type="text"
+            placeholder="Doe"
+          />
+        </label>
       </div>
-    </div>
-    <div class="bg-slate-200 md:w-1/2 md:py-24 md:hidden">
-      <img src="../assets/hero.svg" class="w-auto" />
+
+      <label class="mb-6 w-full tracking-wide">
+        <p class="mb-2 font-bold text-gray-700">EMAIL</p>
+
+        <input
+          class="w-full appearance-none rounded border bg-gray-300 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+          type="text"
+          placeholder="janedoe@gmail.com"
+        />
+      </label>
+
+      <label class="mb-6 w-full tracking-wide">
+        <p class="mb-2 font-bold text-gray-700">YOUR MESSAGE</p>
+
+        <textarea
+          class="w-full resize-none appearance-none rounded border bg-gray-300 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+          rows="4"
+          placeholder="I am ready to growth hack my product to PMF!"
+        ></textarea>
+      </label>
+
+      <button
+        class="w-40 rounded-3xl bg-green-800 px-6 py-2 text-lg text-slate-50"
+      >
+        Submit
+      </button>
     </div>
   </div>
 
-  <div class="bg-slate-100">
-    <div class="bg-slate-100 py-6 md:pt-20 md:w-2/3 pb-0">
-      <p
-        class="text-green-800 text-xl text-center md:px-24 md:text-left md:leading-snug md:text-4xl md:pb-0"
-      >
-        The PMF Survey has been proven successful by companies like
-      </p>
+  <div
+    class="flex flex-row justify-between bg-slate-50 p-10 md:items-center md:px-16 lg:px-32"
+  >
+    <div>
+      <img src="../assets/logo.svg" class="md:w-40" />
     </div>
 
-    <div class="flex flex-row md:justify-evenly pt-0 bg-slate-100 md:align-top">
-      <img src="../assets/dropbox.svg" class="md:w-1/3 w-32" />
-      <img src="../assets/eventbrite.svg" class="md:w-1/3 w-32" />
-      <img src="../assets/superhuman.svg" class="md:w-1/3 w-32" />
-    </div>
-  </div>
+    <div class="flex flex-col md:flex-row md:space-x-3 lg:space-x-6">
+      <a href="" target="_blank" class="inline-block underline">
+        Terms & Conditions
+      </a>
 
-  <div class="md:flex flex-row">
-    <div class="bg-slate-200 py-6 md:py-20 md:w-2/5">
-      <p
-        class="text-green-800 text-xl text-center p-8 pt-3 pb-0 md:px-24 md:text-left md:text-4xl md:leading-snug md:pb-3"
-      >
-        Ready to hack your product growth to PMF? Reach out to us!
-      </p>
-    </div>
+      <a href="" target="_blank" class="inline-block underline">
+        Privacy Policy
+      </a>
 
-    <div class="bg-slate-200 md:px-24 md:py-24 md:w-3/5">
-      <form
-        class="max-w-xs bg-slate-200 md:max-w-2xl pl-14 pb-3 md:pl-0 md:pb-0"
-      >
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
-            >
-              First Name
-            </label>
-            <input
-              class="appearance-none block w-full bg-gray-300 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
-              type="text"
-              placeholder="Jane"
-            />
-            <p class="text-red-500 text-xs italic">
-              Please fill out this field.
-            </p>
-          </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-last-name"
-            >
-              Last Name
-            </label>
-            <input
-              class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-              placeholder="Doe"
-            />
-          </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-password"
-            >
-              Email
-            </label>
-            <input
-              class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="text"
-              placeholder="janedoe@gmail.com"
-            />
-          </div>
-        </div>
-
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-password"
-            >
-              Your message
-            </label>
-            <input
-              class="md:h-36 appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="text"
-              placeholder="I would love to work with you!"
-            />
-          </div>
-        </div>
-        <div class="pl-10 pb-8 md:pl-0">
-          <button
-            class="bg-green-800 text-white py-2 px-7 rounded-3xl w-40 md:h-12"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
-
-  <div class="bg-slate-50">
-    <div class="flex flex-row justify-between items-center md:p-4 p-6 lg:mx-20">
-      <div>
-        <img src="../assets/logo.svg" class="md:w-48" />
-      </div>
-
-      <div class="flex flex-col md:flex-row md:space-x-6">
-        <a href="" target="_blank" class="underline inline-block">
-          Terms & Conditions
-        </a>
-        <a href="" target="_blank" class="underline inline-block">
-          Privacy Policy
-        </a>
-        <a href="/DPN.pdf" target="_blank" class="underline inline-block">
-          PDPA Data Protection Notice
-        </a>
-      </div>
+      <a href="/DPN.pdf" target="_blank" class="inline-block underline">
+        PDPA Data Protection Notice
+      </a>
     </div>
   </div>
 </template>
