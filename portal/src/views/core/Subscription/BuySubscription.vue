d9a9756cf5130b737a7f802026d6b176c48ac0eb
Jaime <jaimeloeuf@gmail.com>
2023-09-02 18:59:14 +0800

    Improve BuySubscription UI.

--- portal/src/views/core/Subscription/BuySubscription.vue
+++ portal/src/views/core/Subscription/BuySubscription.vue
@@ -10,8 +10,7 @@ const moneyFormatter = Intl.NumberFormat("en-US", {
   currency: "SGD",
 }).format;
 
-// @todo Pass in selected plan
-async function buyPlan() {
+async function buyPlan(paymentInterval: "yearly" | "monthly") {
   loader.show();
 
   loader.hide();
@@ -25,14 +24,14 @@ async function buyPlan() {
     </div>
 
     <div
-      class="mx-auto flex max-w-6xl flex-col items-center justify-between gap-6 md:flex-row lg:gap-12"
+      class="mx-auto flex max-w-7xl flex-col items-center justify-between gap-6 md:flex-row lg:gap-12"
     >
       <div class="w-full basis-1/2">
         <div class="p-3 font-light sm:p-6">
           <div class="mb-6 border-b border-gray-300 pb-6">
             <p class="text-xl">
-              Our simple subscription plan charges you a <i>base</i> rate and
-              any <i>extra usage</i> over what is included.
+              Our subscription charges you a <i>base price</i> plus any
+              <i>extra usage</i> over what is included.
             </p>
           </div>
 
@@ -56,7 +55,7 @@ async function buyPlan() {
             <p class="mb-2 text-xl font-normal">
               How much does it cost if I use more than what is included?
             </p>
-            <ul class="list-decimal px-5 text-lg">
+            <ul class="mb-4 list-decimal px-5 text-lg">
               <li>
                 {{ moneyFormatter(PlanDetails.overagePrice.responses) }} /
                 {{ numberFormatter(PlanDetails.overageUnit.responses) }}
@@ -67,6 +66,10 @@ async function buyPlan() {
                 {{ numberFormatter(PlanDetails.overageUnit.emails) }} emails
               </li>
             </ul>
+            <p class="text-sm font-extralight italic">
+              Overage is billed monthly even if you pay for the base
+              subscription yearly.
+            </p>
           </div>
 
           <div>
@@ -89,24 +92,25 @@ async function buyPlan() {
         <div class="flex flex-col gap-6 font-light">
           <div
             class="flex w-full cursor-pointer flex-row items-center justify-between rounded-lg border border-green-600 p-6 hover:shadow-2xl"
-            @click="buyPlan"
+            @click="buyPlan('yearly')"
           >
             <div>
-              <p class="mb-2 text-lg">Pay Yearly</p>
-
-              <p class="mb-4 text-2xl text-green-700">
+              <p class="mb-1 text-3xl text-green-700">
                 {{ moneyFormatter(PlanDetails.price.yearly / 12) }} /
                 <b>Month</b>
               </p>
+              <p class="mb-4 font-extralight">+ Overage</p>
 
               <p>
-                {{ moneyFormatter(PlanDetails.price.yearly) }} billed once a
+                Save
+                <span class="text-green-700"> {{ moneyFormatter(200) }}</span>
+                by paying {{ moneyFormatter(PlanDetails.price.yearly) }} once a
                 year.
               </p>
             </div>
 
             <svg
-              class="h-8 w-8 text-gray-700"
+              class="h-8 w-8 text-green-700"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -124,16 +128,22 @@ async function buyPlan() {
 
           <div
             class="flex w-full cursor-pointer flex-row items-center justify-between rounded-lg border border-gray-300 p-6 hover:shadow-2xl"
-            @click="buyPlan"
+            @click="buyPlan('monthly')"
           >
             <div>
-              <p class="mb-2 text-lg">Pay Monthly</p>
-
-              <p class="mb-4 text-2xl font-extralight">
+              <p class="mb-1 text-2xl font-extralight">
                 {{ moneyFormatter(PlanDetails.price.monthly) }} / Month
               </p>
+              <p class="mb-4 font-extralight">+ Overage</p>
 
-              <p>Get 2 months free by paying once a year instead!</p>
+              <p>
+                Save
+                <span class="text-green-700">
+                  {{ moneyFormatter(200) }}
+                </span>
+                <span class="text-sm"> (2 months free)</span> by paying once a
+                year.
+              </p>
             </div>
 
             <svg
@@ -157,4 +167,3 @@ async function buyPlan() {
     </div>
   </div>
 </template>
-./Stripe/useStripe ./Stripe/utils
