b9cc57d9bf005348042575f07efd38d171dd81b2
Jaime <jaimeloeuf@gmail.com>
2023-10-16 00:42:46 +0800

    Make CreateOneCustomerDTO properties nullable.

--- server/src/app/customer/dto-validation/ValidatedCreateOneCustomerDTO.ts
+++ server/src/app/customer/dto-validation/ValidatedCreateOneCustomerDTO.ts
@@ -1,6 +1,6 @@
 import type { CreateOneCustomerDTO } from 'domain-model';
 
-import { IsString, MaxLength, IsEmail, IsOptional } from 'class-validator';
+import { IsString, MaxLength, IsEmail, ValidateIf } from 'class-validator';
 
 /**
  * The TS not null assertion operator is used as these are supposed to be
@@ -9,23 +9,27 @@ import { IsString, MaxLength, IsEmail, IsOptional } from 'class-validator';
 export class ValidatedCreateOneCustomerDTO implements CreateOneCustomerDTO {
   @IsString()
   @MaxLength(100)
-  @IsOptional()
-  readonly cid!: string;
+  // Allow value to be nullable by skipping all checks if it is null
+  @ValidateIf((_, value) => value !== null)
+  readonly cid!: string | null;
 
   @IsString()
   @MaxLength(120)
-  @IsOptional()
+  // Allow value to be nullable by skipping all checks if it is null
+  @ValidateIf((_, value) => value !== null)
   readonly name!: string | null;
 
   @IsString()
   @MaxLength(254)
   @IsEmail()
-  @IsOptional()
+  // Allow value to be nullable by skipping all checks if it is null
+  @ValidateIf((_, value) => value !== null)
   readonly email!: string | null;
 
   // @todo Add phone validation
   @IsString()
   @MaxLength(30)
-  @IsOptional()
+  // Allow value to be nullable by skipping all checks if it is null
+  @ValidateIf((_, value) => value !== null)
   readonly phone!: string | null;
 }
