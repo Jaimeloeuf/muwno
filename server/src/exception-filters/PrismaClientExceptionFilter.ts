63b1d02afb0271eb977410fe38f2e2b407a9e3aa
Jaime <jaimeloeuf@gmail.com>
2023-10-28 21:51:05 +0800

    Use more specific messages for Prisma exceptions.

--- server/src/exception-filters/PrismaClientExceptionFilter.ts
+++ server/src/exception-filters/PrismaClientExceptionFilter.ts
@@ -2,6 +2,8 @@ import {
   Catch,
   ArgumentsHost,
   Logger,
+  ConflictException,
+  BadRequestException,
   InternalServerErrorException,
 } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
@@ -27,6 +29,23 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   ) {
     this.logger.error(`Code: ${exception.code}`, exception);
 
+    if (exception.code === 'P2002')
+      return super.catch(
+        new ConflictException(
+          `PK contraint error: Failed to create resource as '${exception.meta?.['target']}' needs to be unique`,
+        ),
+        host,
+      );
+
+    if (exception.code === 'P2003')
+      return super.catch(
+        new BadRequestException(
+          `FK contraint error: Failed to create resource as '${exception.meta?.['field_name']}' is invalid`,
+        ),
+        host,
+      );
+
+    // Fallback if the specific code is not dealt with using a custom exception.
     return super.catch(
       new InternalServerErrorException(`DB Exception Code: ${exception.code}`),
       host,
