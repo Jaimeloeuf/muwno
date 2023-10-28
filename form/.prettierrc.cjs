0d38e4ed10bfbbc47fe981025315dd0a505f37b7
Jaime <jaimeloeuf@gmail.com>
2023-10-28 21:38:04 +0800

    Fix and simplify all prettier configs.

--- form/.prettierrc.cjs
+++ form/.prettierrc.cjs
@@ -1,7 +1,5 @@
 module.exports = {
+  // Require is used instead of just "prettier-plugin-tailwindcss" for better
+  // vscode plugin support since it can be ran before file save.
   plugins: [require("prettier-plugin-tailwindcss")],
-
-  endOfLine: "auto",
-  trailingComma: "es5",
-  semi: true,
 };
