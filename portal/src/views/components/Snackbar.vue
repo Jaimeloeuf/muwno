86c282f05157debbd1ca34845eb3288f77e09bed
Jaime <jaimeloeuf@gmail.com>
2023-11-10 13:42:19 +0800

    Implement pause notif countdown on hover.

--- portal/src/views/components/Snackbar.vue
+++ portal/src/views/components/Snackbar.vue
@@ -8,13 +8,8 @@ const numberOfLoops = props.timeout / 10;
 const stepPerLoop = 100 / numberOfLoops;
 
 const progressBarProgress = ref(0);
-
-const progressBarInterval = setInterval(() => {
-  if (progressBarProgress.value >= 99) clearInterval(progressBarInterval);
-  progressBarProgress.value += stepPerLoop;
-}, 10);
-
 const show = ref(true);
+const hover = ref(false);
 
 /**
  * On notification completed, stop showing the snackbar first and do a delay
@@ -22,6 +17,7 @@ const show = ref(true);
  */
 async function completed() {
   show.value = false;
+  hover.value = false;
 
   // Add a delay before showing another notif for the animation of it going down
   // and up again to visually indicate there is a new notif.
@@ -30,11 +26,37 @@ async function completed() {
   emit("close");
 }
 
-setTimeout(completed, props.timeout);
+async function countdown() {
+  while (progressBarProgress.value <= 100) {
+    // Stop the countdown function entirely once user hovers over the snackbar.
+    if (hover.value) return;
+    progressBarProgress.value += stepPerLoop;
+    await new Promise((res) => setTimeout(res, 10));
+  }
+  // Call the completed function once the countdown has completed.
+  completed();
+}
+
+/**
+ * When user stop hovering, update hover tracking variable and call countdown
+ * function to continue the countdown
+ */
+function mouseleave() {
+  hover.value = false;
+  countdown();
+}
+
+// Start countdown at the end of the setup function
+countdown();
 </script>
 
 <template>
-  <div v-if="show" class="fixed bottom-4 right-0 pr-4">
+  <div
+    v-if="show"
+    class="fixed bottom-4 right-0 pr-4"
+    @mouseenter="hover = true"
+    @mouseleave="mouseleave"
+  >
     <div
       class="w-full max-w-xl rounded-lg border border-zinc-200 bg-zinc-50 shadow-2xl drop-shadow-2xl sm:min-w-[24rem]"
     >
