4b62b008b1470229ed68bf39206162c8096ed27e
Jaime <jaimeloeuf@gmail.com>
2023-11-10 16:30:20 +0800

    Remove unused store init function.

--- portal/src/store/init.ts
+++ portal/src/store/init.ts
@@ -1,17 +1,6 @@
 import { useUser } from "./user.store";
 
 /**
- * Use this function to initialize all stores that needs
- * to run their init method after login is successful.
- */
-export async function initStoresOnLoginSuccess(): Promise<void> {
-  console.log(`Running initStoresOnLoginSuccess`);
-
-  // Run all of these initializations without any particular order
-  await Promise.all([]);
-}
-
-/**
  * Use this function to initialize all stores that needs to
  * run their init method on app start if user is logged in.
  *
