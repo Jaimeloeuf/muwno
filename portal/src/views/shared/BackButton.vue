c6ce4018af1395f4293065d59112ac09ea28ceec
Jaime <jaimeloeuf@gmail.com>
2023-11-15 20:49:04 +0800

    Revert some BackButton changes to fix cyclical bug:     Revert BackButton changes which made the BackButton to become a     router-link to always navigate forward, as it causes a bug which     traps users into a cyclical navigation pattern when trying to     navigate backwards because the back route is always the last route     read in history, which means that for navigations with more than 1     back routes chained, it cannot properly recall what to navigate back     to, which causes it to cycle between the last 2 routes.

--- portal/src/views/shared/BackButton.vue
+++ portal/src/views/shared/BackButton.vue
@@ -5,63 +5,50 @@ import { AllProductRoute } from "../../router";
 
 const props = defineProps<{
   /**
+   * ## WARNING
+   * **Do not use this** unless absolutely neccessary. Since this will cause a
+   * forward navigation with `router.push` and if you navigate forward to a page
+   * with a back button, it will come back to this page on click which might
+   * cause cyclical back navigation routes! Only use this if you are navigating
+   * back to a page with no back button.
+   *
    * Custom back route can be specified instead of just going back one step in
-   * navigation history. If specified, it will be pass to the router-link
-   * component's `to` prop.
+   * navigation history.
    */
   to?: RouteLocationRaw | undefined;
 }>();
 
 const router = useRouter();
 const notif = useNotif();
-let noBackRoute = false;
 
 /**
- * Resolves the `backRoute` where the app will navigate to on clicking the back
- * button depending on values passed in and the current router state.
+ * Navigate backwards using values passed in and the current router state.
  */
-function resolveRoute() {
+function back() {
   // Whatever route the caller specifies always take priority
-  if (props.to !== undefined) return router.resolve(props.to);
+  if (props.to !== undefined) {
+    router.push(props.to);
+  }
 
   // Use the last route user navigated from as the back route
-  if (
+  else if (
     router.options.history.state.back !== null &&
     typeof router.options.history.state.back === "string"
-  )
-    return router.resolve({ path: router.options.history.state.back });
+  ) {
+    router.back();
+  }
 
-  // If current page is the first route opened from a new opened tab or from
-  // another website/webapp, route will resolve to the default Home page. This
-  // also sets `noBackRoute` to notify user about this on navigation.
-  noBackRoute = true;
-  return router.resolve({ name: AllProductRoute.name });
+  // If there is no back route, i.e. user opened current page from a new tab /
+  // another website, then redirect to the default back route, a.k.a Home page.
+  else {
+    router.push({ name: AllProductRoute.name });
+    notif.setSnackbar("No back page found, returning to Home Page");
+  }
 }
-
-/** Route to navigate to on clicking back */
-const backRoute = resolveRoute();
-
-/**
- * Function to notify user about redirecting to Home page if there is no valid
- * back route found and they clicked back.
- */
-const onBackNavigation = () =>
-  noBackRoute &&
-  notif.setSnackbar("No back page found, returning to Home Page");
 </script>
 
 <template>
-  <!--
-    Using router link means it will always be a forward navigation by adding a
-    new navigation to the history state. Thid does not navigate back using
-    history state, to give users the ability to traverse through time all the
-    navigation steps taken.
-  -->
-  <router-link
-    :to="backRoute"
-    class="rounded-lg bg-zinc-100 px-4 py-1.5"
-    @click="onBackNavigation"
-  >
+  <button class="rounded-lg bg-zinc-100 px-4 py-1.5" @click="back">
     <svg
       class="inline-block h-6 w-6 rotate-180 text-zinc-600"
       aria-hidden="true"
@@ -77,5 +64,5 @@ const onBackNavigation = () =>
         d="M1 5h12m0 0L9 1m4 4L9 9"
       />
     </svg>
-  </router-link>
+  </button>
 </template>
