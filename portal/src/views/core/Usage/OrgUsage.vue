77a4ebdf06eaea973b2164e319de61788f14a20d
Jaime <jaimeloeuf@gmail.com>
2023-11-21 19:00:37 +0800

    Update OrgUsage view to call API.

--- portal/src/views/core/Usage/OrgUsage.vue
+++ portal/src/views/core/Usage/OrgUsage.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import { sf } from "simpler-fetch";
+import { getAuthHeader } from "../../../firebase";
 import { flags } from "../../../utils/flags";
 import TopNavbar from "../../shared/TopNavbar.vue";
+import type { ReadUsageDTO } from "@domain-model";
+
+async function getUsage() {
+  const { res, err } = await sf
+    .useDefault()
+    .GET(`/usage/org`)
+    .useHeader(getAuthHeader)
+    .runJSON<ReadUsageDTO>();
+
+  if (err) throw err;
+  if (!res.ok)
+    throw new Error(`Failed to get Usage stats: ${JSON.stringify(res)}`);
+
+  return res.data.usage;
+}
+
+const usage = await getUsage();
 </script>
 
 <template>
@@ -8,12 +27,24 @@ import TopNavbar from "../../shared/TopNavbar.vue";
     <TopNavbar sideDrawer>Organisation Usage</TopNavbar>
 
     <div class="mx-auto flex max-w-2xl flex-col gap-6">
-      <p class="text-2xl font-light">
-        Usage across entire Organisation this billing cycle
-      </p>
+      <p class="text-2xl font-light">Organisation Usage</p>
 
       <div>
         <p>Responses processed</p>
+        <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
+          {{ usage.response ?? "-" }}
+        </p>
+      </div>
+
+      <div>
+        <p>Emails sent</p>
+        <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
+          {{ usage.emailsSent ?? "-" }}
+        </p>
+      </div>
+
+      <div v-if="flags.devMode">
+        <p>SMS sent</p>
         <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
           {{ "-" }}
         </p>
@@ -22,26 +53,12 @@ import TopNavbar from "../../shared/TopNavbar.vue";
       <div>
         <p>Responses stored</p>
         <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
-          {{ "-" }}
-        </p>
-      </div>
-
-      <div>
-        <p>Customers stored</p>
-        <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
-          {{ "-" }}
-        </p>
-      </div>
-
-      <div>
-        <p>Emails sent</p>
-        <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
-          {{ "-" }}
+          {{ usage.responseStored ?? "-" }}
         </p>
       </div>
 
       <div v-if="flags.devMode">
-        <p>SMS sent</p>
+        <p>Customers stored</p>
         <p class="rounded-lg border border-zinc-200 bg-zinc-50 p-2">
           {{ "-" }}
         </p>
