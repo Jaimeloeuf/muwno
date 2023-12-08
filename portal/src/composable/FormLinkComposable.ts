2156598b5c620d9045c0270eaa60ef7623fea712
Jaime <jaimeloeuf@gmail.com>
2023-12-09 01:08:45 +0800

    Fix FormLinkComposable and remove standalone utils.

--- portal/src/composable/FormLinkComposable.ts
+++ portal/src/composable/FormLinkComposable.ts
@@ -1,20 +1,30 @@
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { isLinkValidReactive } from "../utils/isLinkValid";
-import { getReactiveSurveyLinkWithRedirect } from "../utils/getSurveyLink";
+import { getSurveyLink } from "../utils/getSurveyLink";
 import type { ProductID } from "@domain-model";
 
+/**
+ * Composable to manage form redirectLink and the survey link itself.
+ */
 export function useFormLinks(productID: ProductID) {
-  /**  */
+  /**
+   * Redirect link for user to set through 2 way binded inputs.
+   */
   const redirectLink = ref("");
 
   const isRedirectLinkValid = isLinkValidReactive(redirectLink);
 
-  const surveyLink = getReactiveSurveyLinkWithRedirect(
-    productID,
-    isRedirectLinkValid,
-    redirectLink
+  /**
+   * `surveyLink` only includes the redirect link if it is valid.
+   */
+  const surveyLink = computed(
+    () =>
+      `${getSurveyLink(productID)}${
+        isRedirectLinkValid.value
+          ? `?redirect=${encodeURIComponent(redirectLink.value)}`
+          : ""
+      }`
   );
 
-  // expose managed state as return value
   return { redirectLink, isRedirectLinkValid, surveyLink };
 }
