09da68a7f97dbb9d099c7b355227a976815b1586
Jaime <jaimeloeuf@gmail.com>
2023-12-13 22:54:14 +0800

    Update CopyOnClick to use errorStore.

--- portal/src/views/shared/CopyOnClick.vue
+++ portal/src/views/shared/CopyOnClick.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { useSlots } from "vue";
-import { useNotif } from "../../store";
+import { useNotif, useError } from "../../store";
 
 const notif = useNotif();
+const errorStore = useError();
 
 const slots = useSlots();
 const props = defineProps<{ textToCopy?: string }>();
@@ -14,14 +15,18 @@ async function copy() {
   // Allow user to copy the slot content directly
   // But this requires a runtime check to ensure that either a string is
   // passed in or the slot is defined since it cant be done statically.
-  const copyString = props.textToCopy
-    ? props.textToCopy
-    : slots.default && slots.default()[0]?.children?.toString();
+  const copyString =
+    props.textToCopy !== undefined
+      ? props.textToCopy
+      : slots.default && slots.default()[0]?.children?.toString();
 
-  if (!copyString) alert("RUNTIME ERR: CopyOnClick copy string is empty!");
+  if (copyString === undefined || copyString === "") {
+    errorStore.newError("RUNTIME ERR: CopyOnClick copyString is empty!");
+    return;
+  }
 
   await navigator.clipboard
-    .writeText(copyString || "")
+    .writeText(copyString)
     .then(() => notif.setSnackbar("Copied!"))
     .catch(() => notif.setSnackbar("Failed to copy!"));
 }
