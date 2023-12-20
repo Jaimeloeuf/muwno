3bfbfbce26a3469530c9d9543a37c4f26078a317
Jaime <jaimeloeuf@gmail.com>
2023-12-21 00:32:30 +0800

    Misc fixes: 1.  Remove unnecessary scaffolded UI in TeamPendingInvites. 2.  Add missing controller decorator.

--- portal/src/views/core/Team/TeamPendingInvites.vue
+++ portal/src/views/core/Team/TeamPendingInvites.vue
@@ -2,22 +2,17 @@
 import { ref } from "vue";
 import { sf } from "simpler-fetch";
 import { getAuthHeader } from "../../../firebase";
-import { useUser, useLoader, useError } from "../../../store";
-import { InviteMemberRoute } from "../../../router";
+import { useLoader, useError } from "../../../store";
 import { useSearch } from "../../../composable";
 import TopNavbar from "../../shared/TopNavbar.vue";
 import Accordion from "../../shared/Accordion.vue";
 import DeletePendingInviteButton from "./DeletePendingInviteButton.vue";
 import { getDateString } from "../../../utils/date-formatting/getDateString";
-import { Role, roleMapper } from "@domain-model";
+import { roleMapper } from "@domain-model";
 import type { ReadManyTeamMemberInvitationDTO } from "@domain-model";
 
-const userStore = useUser();
 const loader = useLoader();
 const errorStore = useError();
-
-const user = await userStore.getUser();
-const userIsAdmin = user.role === Role.OrgOwner || user.role === Role.OrgAdmin;
 
 async function getInvites() {
   const { res, err } = await sf
@@ -66,58 +61,31 @@ async function reloadInvites() {
 
     <div class="mx-auto max-w-4xl">
       <div class="flex flex-row items-center justify-between">
-        <p class="pb-4 text-xl">Team Members ({{ invites.length }})</p>
+        <p class="pb-4 text-xl">Pending invitations ({{ invites.length }})</p>
       </div>
 
-      <div
-        class="flex flex-col items-center justify-between gap-3 pb-6 md:flex-row"
-      >
-        <div class="flex w-full max-w-xs flex-row gap-3">
-          <input
-            ref="searchField"
-            v-model.trim="searchInput"
-            type="text"
-            class="w-full rounded-lg border border-zinc-200 p-2 px-4 focus:outline-none"
-            placeholder="Search by Name"
-          />
+      <div class="flex w-full max-w-xs flex-row gap-3 pb-6">
+        <input
+          ref="searchField"
+          v-model.trim="searchInput"
+          type="text"
+          class="w-full rounded-lg border border-zinc-200 p-2 px-4 focus:outline-none"
+          placeholder="Search by Email"
+        />
 
-          <button
-            class="rounded-lg bg-zinc-100 px-4 text-sm font-light text-zinc-900"
-            @click="clearSearchInput"
-          >
-            clear
-          </button>
-        </div>
-
-        <router-link
-          v-if="userIsAdmin"
-          :to="{ name: InviteMemberRoute.name }"
-          class="inline-flex w-full max-w-xs items-center justify-between rounded-lg border border-green-600 px-4 py-1 text-green-600"
+        <button
+          class="rounded-lg bg-zinc-100 px-4 text-sm font-light text-zinc-900"
+          @click="clearSearchInput"
         >
-          <div>Invite Team Member</div>
-          <svg
-            class="h-8 w-8"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </router-link>
+          clear
+        </button>
       </div>
 
       <p
         v-if="searchInput !== '' && results.length === 0"
         class="text-2xl font-thin"
       >
-        No user matches your search.
+        No invitation matches your search.
       </p>
 
       <div class="flex flex-col gap-4">
