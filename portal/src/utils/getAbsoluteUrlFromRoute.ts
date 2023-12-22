1ea829660fba1008a2406617129f5d3f2394135b
Jaime <jaimeloeuf@gmail.com>
2023-12-22 23:28:29 +0800

    Change import to fix build warnings:     Build warnings of potential circular dependency issues fixed by     specifying the Router path directly.

--- portal/src/utils/getAbsoluteUrlFromRoute.ts
+++ portal/src/utils/getAbsoluteUrlFromRoute.ts
@@ -1,5 +1,5 @@
 import type { RouteLocationRaw } from "vue-router";
-import { router } from "../router";
+import { router } from "../router/Router";
 
 /**
  * Get an Absolute URL of the given Route object based on the current relative
