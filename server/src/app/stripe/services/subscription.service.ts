f57b888a2350c529abc77253d86a49dfa7718aed
Jaime <jaimeloeuf@gmail.com>
2023-12-23 21:37:40 +0800

    Metered product's current subscription period:     Add method to get metered product's current subscription time period     so that other service methods like getting usage data can be built     on top of this.

--- server/src/app/stripe/services/subscription.service.ts
+++ server/src/app/stripe/services/subscription.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 import { StripeClient } from '../infra/stripe-client.js';
+import { IStripeCustomerRepo } from '../../../DAL/index.js';
+
+// Service layer Exceptions
+import { InvalidInputException } from '../../../exceptions/index.js';
+
+// Utils
+import { unixSecondsToIsoString } from '../../../utils/index.js';
 
 @Injectable()
 export class StripeSubscriptionService {
-  constructor(private readonly stripe: StripeClient) {}
+  constructor(
+    private readonly stripe: StripeClient,
+    private readonly stripeCustomerRepo: IStripeCustomerRepo,
+  ) {}
 
   /**
    * Check if a given Stripe coupon is valid.
@@ -21,5 +31,30 @@ export class StripeSubscriptionService {
     } catch (error) {
       return { valid: false };
     }
+  }
+
+  /**
+   * Get current billing period timestamps (ISO Date Time String) of an Org's
+   * metered product.
+   */
+  async getCurrentPeriodOfMeteredSubscription(orgID: string) {
+    const customer = await this.stripeCustomerRepo.getCustomerWithOrgID(orgID);
+    if (customer === null)
+      throw new InvalidInputException(
+        `Cannot read metered product subscription period as Org ${orgID} does not have a Stripe Account`,
+      );
+    if (customer.meteredSubscriptionID === null)
+      throw new Error(
+        `Cannot read metered product subscription period as Org ${orgID} does not have a metered subscription`,
+      );
+
+    const stripeSubscription = await this.stripe.subscriptions.retrieve(
+      customer.meteredSubscriptionID,
+    );
+
+    return {
+      from: unixSecondsToIsoString(stripeSubscription.current_period_start),
+      to: unixSecondsToIsoString(stripeSubscription.current_period_end),
+    };
   }
 }
