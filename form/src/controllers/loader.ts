911336ebe128ebfeffab968af14d4fc980ad60fb
Jaime <jaimeloeuf@gmail.com>
2024-01-02 03:19:55 +0800

    Update form/ loader controller:     Copy over the updates from landing/ loader controller to ensure that     it will not error out on the same issue.

--- form/src/controllers/loader.ts
+++ form/src/controllers/loader.ts
@@ -1,21 +1,21 @@
 import { ref } from "vue";
 
-class LoaderController {
-  /**
-   * Boolean flag to control if Loader should be shown
-   */
-  showLoader = ref(false);
-
-  show() {
-    this.showLoader.value = true;
-  }
-
-  hide() {
-    this.showLoader.value = false;
-  }
-}
+/**
+ * Boolean flag to control if Loader should be shown
+ */
+const showLoader = ref(false);
+const show = () => (showLoader.value = true);
+const hide = () => (showLoader.value = false);
 
 /**
- * Use a single shared instance of `LoaderController`
+ * Doing this instead of using a singleton class / literal object for
+ * definitions since using the methods in vue template will fail as using it for
+ * `@click` for example messes with the `this` binding which causes it to fail
+ * when trying to access the showLoader variable.
+ *
+ * @todo
+ * There is no use for this right now since there is no script controlled
+ * loading triggers in form/ right now. Even submit is instant UI change and the
+ * API call runs in the background without needing to show user the loader.
  */
-export const loader = new LoaderController();
+export const loader = { showLoader, show, hide };
