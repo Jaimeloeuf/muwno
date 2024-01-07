b5650765337456acb4c09aecdc0dc4e53d26b0aa
Jaime <jaimeloeuf@gmail.com>
2024-01-08 00:21:15 +0800

    Reload on failed dynamic import in portal router:     Update router error handler to check if the routing error is caused     by failed dynamic import of the Route's component. If so, assume     that the reason for that is because of version skewing, where users     are still using the old version after we deployed a new version, and     reload the page or trigger the browser to load the new route directly     instead of loading it dynamically.

--- portal/src/router/Router.ts
+++ portal/src/router/Router.ts
@@ -31,9 +31,31 @@ router.beforeEach(routeGuard);
  * Once the error is caught, it is set on the reactive `routerError` which is
  * used by the root App.vue to show the error page instead of showing the
  * default loader screen indefinitely.
+ *
+ * @todo Add a APM tool like sentry or something
  */
-router.onError((error) => {
+router.onError((error, to) => {
   console.error("RouterError: ", error);
+
+  // If error message specifies failed dynamic import of the Route's component,
+  // assume that the error is caused by version drift/skew, where users are
+  // still using the old version after a new version is deployed. Attempt to
+  // solve it by reloading the page or triggering the browser to load the new
+  // route directly by navigating there instead of loading it dynamically.
+  //
+  // Error messages are usually either "failed to fetch dynamically imported
+  // module" or "TypeError: error loading dynamically imported module".
+  if (
+    error.message.lowercase().includes("importing a module script failed") ||
+    error.message.lowercase().includes("dynamically imported module")
+  ) {
+    // The route might not have a full path, so reload page if not found.
+    if (to?.fullPath) window.location.href = to.fullPath;
+    else window.location.reload();
+    return;
+  }
+
+  // If it is an unknown error, show user with <GlobalError> view.
   routerError.value = error;
 });
 
