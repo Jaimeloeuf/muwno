c96f979c94cdecfd82bae39e3d960c69b5441399
Jaime <jaimeloeuf@gmail.com>
2024-01-07 20:50:29 +0800

    Beautify Snackbar notif close button.

--- portal/src/views/shared/Snackbar/Card.vue
+++ portal/src/views/shared/Snackbar/Card.vue
@@ -57,14 +57,27 @@ countdown();
     @mouseenter="hover = true"
     @mouseleave="mouseleave"
   >
-    <div class="flex flex-row justify-between p-4">
+    <div class="flex flex-row items-center justify-between p-4">
       <p class="pr-6">{{ msg }}</p>
       <button
-        type="button"
-        class="whitespace-nowrap font-medium text-zinc-700 underline underline-offset-2"
+        class="inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white px-2 py-1 text-zinc-400 shadow"
         @click="completed"
       >
-        Ok
+        <svg
+          class="h-3 w-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
       </button>
     </div>
 
