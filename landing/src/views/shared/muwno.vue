f318fe60a3568d1de769c3840530e60b056b455e
Jaime <jaimeloeuf@gmail.com>
2024-01-13 01:33:48 +0800

    Fix landing muwno image word height:     Fix alignment issues when using that on bigger screens.

--- landing/src/views/shared/muwno.vue
+++ landing/src/views/shared/muwno.vue
@@ -1,6 +1,6 @@
 <template>
   <img
     src="../../assets/logo.svg"
-    class="inline-block h-6 px-1 pb-1 md:h-7 lg:h-10 lg:pb-2"
+    class="inline-block h-6 px-1 pb-1 md:h-7 lg:h-9 lg:pb-2"
   />
 </template>
