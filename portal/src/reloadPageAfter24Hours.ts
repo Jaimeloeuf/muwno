d21df0cf04467ab863030eb5d86db91301492363
Jaime <jaimeloeuf@gmail.com>
2024-01-13 20:30:50 +0800

    Add reload portal script to prevent version skew.

---
+++ portal/src/reloadPageAfter24Hours.ts
@@ -0,0 +1,8 @@
+import { reloadPage } from "./utils/reloadPage";
+
+/**
+ * Sets a timer to automatically call `reloadPage` after 24 hours, as a simple
+ * mechanism to prevent version skewing if somehow the user keeps the app open
+ * for a long time.
+ */
+setTimeout(reloadPage, 8.64e7);
