7bbcd34547a1cff3729af71855062206cde23fc3
Jaime <jaimeloeuf@gmail.com>
2024-01-17 00:11:48 +0800

    Add enter key to login for email field:     Since most users will use browser's password manager for email and     password autofill, and browser will keep cursor/focus on email field     even after autofilling both the email and password field. It is     easier to have the enter key to login on email field too than getting     users to manually click the sign in button.

--- portal/src/views/core/Auth/Login.vue
+++ portal/src/views/core/Auth/Login.vue
@@ -118,6 +118,7 @@ async function forgetPassword() {
           type="text"
           class="w-full rounded-md border border-zinc-200 bg-zinc-50 px-3 py-2 focus:outline-none"
           placeholder="janedoe@business.com"
+          @keydown.enter="login"
         />
       </label>
     </div>
