61b1d436bff1162cdc8ffbb9106ad76d2fa7a6ed
Jaime <jaimeloeuf@gmail.com>
2024-01-19 02:13:01 +0800

    Load AllProduct view's live score in background:     So that it will not block initial page load since it is wrapped in     Suspense block, as Org's with lots of products will take quite some     time calculating all the live scores.

--- app/src/views/core/Product/AllProduct/SimplePMFLiveScoreCard.vue
+++ app/src/views/core/Product/AllProduct/SimplePMFLiveScoreCard.vue
@@ -1,53 +1,71 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useCacheableLiveScore } from "../../../../composable";
 import type { ProductID } from "@domain-model";
 
 const props = defineProps<{ productID: ProductID; cacheKey: string }>();
 
-const { PMFScore, reliability } = await useCacheableLiveScore(
-  props.productID,
-  props.cacheKey
+const liveScore = ref<Awaited<ReturnType<typeof useCacheableLiveScore>> | null>(
+  null
+);
+
+// Call API and let it run in the background to complete page load faster.
+useCacheableLiveScore(props.productID, props.cacheKey).then(
+  (v) => (liveScore.value = v)
 );
 </script>
 
 <template>
   <div
-    class="rounded-lg border border-zinc-200 bg-white p-4 font-light"
-    :class="{ 'w-full max-w-xs': PMFScore.score !== null }"
+    class="w-full max-w-xs rounded-lg border border-zinc-200 bg-white p-4 font-light"
   >
-    <p v-if="PMFScore.score === null" class="text-lg font-extralight">
-      No live score yet,
-      <br />
-      waiting for responses.
-    </p>
+    <div
+      v-if="liveScore === null"
+      class="flex h-full flex-col items-center justify-center text-xl font-thin"
+    >
+      <p>... loading ...</p>
+    </div>
 
     <template v-else>
-      <p class="text-lg">
-        Live Score,
-        <span
-          :class="{
-            'text-lime-700': PMFScore.score >= 40,
-            'text-yellow-700': PMFScore.score < 40 && PMFScore.score >= 30,
-            'text-red-700': PMFScore.score < 30,
-          }"
-        >
-          {{ PMFScore.score }}
-        </span>
+      <p
+        v-if="liveScore.PMFScore.score === null"
+        class="text-lg font-extralight"
+      >
+        No live score yet,
+        <br />
+        waiting for responses.
       </p>
 
-      <p>
-        <span
-          :class="{
-            'text-lime-600':
-              reliability === 'Very reliable' || reliability === 'Reliable',
-            'text-yellow-700': reliability === 'Somewhat reliable',
-            'text-red-700': reliability === 'Less reliable',
-          }"
-        >
-          {{ reliability }}
-        </span>
-        with {{ PMFScore.totalResponses }} responses.
-      </p>
+      <template v-else>
+        <p class="text-lg">
+          Live Score,
+          <span
+            :class="{
+              'text-lime-700': liveScore.PMFScore.score >= 40,
+              'text-yellow-700':
+                liveScore.PMFScore.score < 40 && liveScore.PMFScore.score >= 30,
+              'text-red-700': liveScore.PMFScore.score < 30,
+            }"
+          >
+            {{ liveScore.PMFScore.score }}
+          </span>
+        </p>
+
+        <p>
+          <span
+            :class="{
+              'text-lime-600':
+                liveScore.reliability === 'Very reliable' ||
+                liveScore.reliability === 'Reliable',
+              'text-yellow-700': liveScore.reliability === 'Somewhat reliable',
+              'text-red-700': liveScore.reliability === 'Less reliable',
+            }"
+          >
+            {{ liveScore.reliability }}
+          </span>
+          with {{ liveScore.PMFScore.totalResponses }} responses.
+        </p>
+      </template>
     </template>
   </div>
 </template>
